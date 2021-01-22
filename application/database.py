1c11d56373fff1147603c4426f91882c19b14af2
Kelly Foulk <klfoulk16@gmail.com>
2021-01-22 18:51:14 -0500

    Initial Commit

---
+++ application/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String
+from application import db
+
+
+class User(db.Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True)
+    password = Column(String, unique=True)
+
+    def __init__(self, username=None, password=None):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
